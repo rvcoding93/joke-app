e95f6d26f44eb6aef557d11799f353c2da7b7b5d
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-13 09:35:26 -0400

    ecomm inital commit

---
+++ react/shopper/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Nav = () => {
+    <nav className="App-nav">
+        <ul>
+            <li className="App-nav-item">
+                <button>Items</button>
+            </li>
+            <li className="App-nav-item">
+                <button>Cart</button>
+            </li>
+        </ul>
+    </nav>
+}
+
+export default Nav;
